a978a55d9153ea8bd8574f772b304c388e7dfc9c
Isaac <110391240+Isaac-Miless@users.noreply.github.com>
2023-11-10 14:36:06 +0000

    Fix Web Server

--- comments.js
+++ comments.js
@@ -1,15 +1,51 @@
+// create web server
+import { Router } from 'express';
+var router = Router();
 
-const http = require('http');
+// load mongoose module
+import { Schema, model, connect } from 'mongoose';
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+// create mongoose schema
+var commentSchema = Schema({
+  name: String,
+  comment: String,
+  created_at: Date
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+// create mongoose model
+var Comment = model('Comment', commentSchema);
+
+// connect mongoose
+connect('mongodb://localhost:27017/comment');
+
+// create route
+// GET /comments
+router.get('/', function(req, res, next) {
+  // find all comments
+  Comment.find(function(err, comments) {
+    if (err) return res.status(500).send({error: 'database failure'});
+    res.render('comments', {comments: comments});
+  });
 });
+
+// POST /comments
+router.post('/', function(req, res, next) {
+  // create new comment
+  var comment = new Comment();
+  comment.name = req.body.name;
+  comment.comment = req.body.comment;
+  comment.created_at = new Date();
+
+  // save comment
+  comment.save(function(err) {
+    if (err) {
+      console.error(err);
+      res.json({result: 0});
+      return;
+    }
+    res.redirect('/comments');
+  });
+});
+
+// export router
+export default router;
